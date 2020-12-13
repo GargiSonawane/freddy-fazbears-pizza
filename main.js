1348a927bb3b45d6e2e8537d232bd00d00fae33a
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2020-12-13 11:48:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function add_item(){
     food_array.sort();
     htmldata="<section class='cards'>"
     for(var i=0;i<food_array.length;i++){
-        htmldata=htmldata+'<div class="card">'+'<img src="images/pizzaImg.png">'+ food_array[i]+ '</div>'
+        htmldata=htmldata+'<div class="card">'+'<img src="pizzaImg.png">'+ food_array[i]+ '</div>'
     }
     htmldata=htmldata+"</section>"
     document.getElementById("display_addedmenu").innerHTML=htmldata;
